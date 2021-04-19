50d65dc27e87ef62b700557bfc91028d4b6efdca
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-19 20:08:34 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,61 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: https://gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue'
+import '~/assets/css/base.postcss'
+
+export default function(Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+
+  head.htmlAttrs = { lang: 'en' }
+  head.bodyAttrs = { class: 'antialiased font-body font-serif' }
+
+  // Styles
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Great+Vibes|Libre+Baskerville&display=swap',
+  })
+
+  // Basic meta tags
+  head.meta.push({
+    name: 'author',
+    content: 'Lennart Müller and Lukas Langrock',
+  })
+
+  head.meta.push({
+    name: 'keywords',
+    content: 'Nigeria,History,Africa,English',
+  })
+
+  head.meta.push({
+    name: 'description',
+    content: 'School project about the history of Nigeria.',
+  })
+
+  // Open Graph + Twitter meta tags
+  head.meta.push({
+    property: 'og:description',
+    content: 'School project about the history of Nigeria.',
+  })
+
+  head.meta.push({
+    name: 'twitter:description',
+    content: 'School project about the history of Nigeria.',
+  })
+
+  head.meta.push({
+    property: 'og:type',
+    content: 'website',
+  })
+
+  head.meta.push({
+    property: 'og:title',
+    content: 'History of Nigeria - A School Project Website',
+  })
+
+  head.meta.push({
+    name: 'twitter:title',
+    content: 'Tulip - Single Page Gridsome Starter',
+  })
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <Layout>
+    <!-- Replace everything inside this Layout with your own content -->
+    <!-- The main container CSS classes are in `src/layouts/Default.vue` -->
+
+    <div class="mt-8 md:mt-16 flex flex-wrap items-center justify-center">
+      <div>
+        <h1 class="font-maintitle text-5xl sm:text-7xl block text-center sm:pr-8 text-gray-700">
+          {{ $static.metadata.siteName }}
+        </h1>
+      </div>
+
+      <g-image
+        class="block"
+        alt="Tulip"
+        src="~/assets/img/ovinuchi-ejiohuo-OxesnxkySD0-unsplash.jpg"
+      />
+    </div>
+
+    <h2>Coming soon...</h2>
+    <br />
+
+    <p class="text-center mt-16 mb-8 text-xs">
+      Thanks to Ville Säävuori for making this great template ❤️ <br />
+      Moderated by Lennart Schröder <br />
+      Hosted by Lukas Langrock - <a
+        class="underline text-blue-700"
+        href="https://langrock.info/imprint"
+      >Imprint</a> - <a
+        class="underline text-blue-700"
+        href="https://langrock.info/privacy"
+      >Privacy Policy</a> <br />
+      &copy; Ville Säävuori (<a
+        class="underline text-blue-700"
+        href="https://twitter.com/uninen"
+      >@uninen</a>) 2020, MIT License
+    </p>
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "History of Nigeria"
+  }
+};
+</script>
