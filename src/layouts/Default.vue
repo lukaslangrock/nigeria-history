fc5ec44a018616832dc321e4bdaae811226da60b
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-20 16:27:15 +0200

    Update footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,23 +3,22 @@
   <div class="container max-w-3xl px-4 mx-auto">
     <slot />
     <p class="text-center mt-16 mb-8 text-xs">
-      Thanks to Ville Säävuori for making this great template ✨<br />
+      Starter template made by Ville Säävuori ✨<br />
       Moderated by Lennart Schröder 📝<br />
-      Hosted by Lukas Langrock 💻 - <a
-        class="underline text-green-700"
-        href="https://langrock.info/imprint"
-      >Imprint</a> - <a
-        class="underline text-green-700"
-        href="https://langrock.info/privacy"
-      >Privacy Policy</a> <br />
+      Hosted by Lukas Langrock 💻
+      <br />
+      <br /><g-link to="/about/">About</g-link>
+       - <a class="underline text-green-700" href="https://langrock.info/imprint">Imprint</a>
+       - <a class="underline text-green-700" href="https://langrock.info/privacy">Privacy Policy</a>
+      <br />
       &copy; Ville Säävuori (<a
         class="underline text-green-700"
         href="https://twitter.com/uninen"
-      >@uninen</a>) 2020, MIT License <br />
+        >@uninen</a>) 2020, MIT License <br />
       &copy; <a
         class="underline text-green-700"
         href="https://fontawesome.com/license/free"
-      >Font Awesome Free License</a>, CC BY 4.0
+        >Font Awesome Free License</a>, CC BY 4.0
     </p>
   </div>
 </template>
