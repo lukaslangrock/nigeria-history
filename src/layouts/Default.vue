dccc43f5caeb39816f62a2218e7f92156d367ffd
Lukas Langrock <lukas.langrock@mailbox.org>
2021-04-20 12:55:09 +0200

    Make footer links green

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,18 +6,18 @@
       Thanks to Ville Säävuori for making this great template ✨<br />
       Moderated by Lennart Schröder 📝<br />
       Hosted by Lukas Langrock 💻 - <a
-        class="underline text-blue-700"
+        class="underline text-green-700"
         href="https://langrock.info/imprint"
       >Imprint</a> - <a
-        class="underline text-blue-700"
+        class="underline text-green-700"
         href="https://langrock.info/privacy"
       >Privacy Policy</a> <br />
       &copy; Ville Säävuori (<a
-        class="underline text-blue-700"
+        class="underline text-green-700"
         href="https://twitter.com/uninen"
       >@uninen</a>) 2020, MIT License <br />
       &copy; <a
-        class="underline text-blue-700"
+        class="underline text-green-700"
         href="https://fontawesome.com/license/free"
       >Font Awesome Free License</a>, CC BY 4.0
     </p>
